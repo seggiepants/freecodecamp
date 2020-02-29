847a575ef7eb714ddbc9532bd5c4970aec8e5751
seggiepants <41271733+seggiepants@users.noreply.github.com>
2020-02-29 00:23:37 -0700

    broke stuff trying to add in redux - doh!

--- front end libraries/markdown-previewer/src/index.js
+++ front end libraries/markdown-previewer/src/index.js
@@ -1,6 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import Redux from 'redux';
+import ReactRedux from 'react-redux';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const SOURCE = 'Source';
+
+const modifySource = (value) => {
+  return {
+    type: SOURCE,
+    source: value,
+  };
+};
+
+const messageReducer = (state = '', action) => {
+  switch(action.type) {
+    case SOURCE:
+      return action.source;
+    default:
+      return state;
+  };
+};
+
+const store = Redux.createStore(messageReducer);
+const Provider = ReactRedux.Provider;
+const connect = ReactRedux.connect;
+
+const mapStateToProps = (state) => {
+  return {source: state}
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    submitNewMessage: (value) => {
+      dispatch(modifySource(value))
+    }
+  }
+};
+
+const Container = connect(mapStateToProps, mapDispatchToProps)(App);
+ReactDOM.render(<Provider store={store}><Container /></Provider>, document.getElementById('root'));
