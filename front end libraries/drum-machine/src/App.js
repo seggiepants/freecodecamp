87e2a72ef4dd98ffe159234072ec3a8b38290065
seggiepants <41271733+seggiepants@users.noreply.github.com>
2020-03-04 00:09:25 -0700

    Make things pretty, then I decided to just turn it in.

--- front end libraries/drum-machine/src/App.js
+++ front end libraries/drum-machine/src/App.js
@@ -60,7 +60,6 @@ class App extends React.Component {
         mp3: "step.mp3",
       },
     }
-    console.log(Object.keys(this.drums));
 
     this.playAudio = this.playAudio.bind(this);
     this.handleKey = this.handleKey.bind(this);
@@ -99,7 +98,7 @@ class App extends React.Component {
     let buttons = [];
     for(let i = 0; i < keys.length; i++) {
       let key = keys[i];
-      buttons.push(<button id={this.drums[key].id} className="drum-pad" onClick={() => this.playAudio(key)}>
+      buttons.push(<button id={this.drums[key].id} key={this.drums[key].id} className="drum-pad btn btn-info col-sm-3" onClick={() => this.playAudio(key)}>
         {key}
         <audio id={key} className="clip" src={audio_root + this.drums[key].ogg}>
           <source src={audio_root + this.drums[key].mp3} />
@@ -107,21 +106,13 @@ class App extends React.Component {
         </audio>
       </button>);
     }
-    /*
-        <button id="btnAlert" className="drum-pad" onClick={() => this.playAudio("Q")}>Q<audio id="Q" className="clip" src="audio/alert.ogg"><source src="audio/alert.mp3" /><source src="audio/alert.ogg" /></audio></button>
-        <button id="btnCoin" className="drum-pad" onClick={() => this.playAudio("W")}>W<audio id="W" className="clip" src="audio/coin.ogg"><source src="audio/coin.mp3" /><source src="audio/coin.ogg" /></audio></button>
-        <button id="btnExplosion" className="drum-pad" onClick={() => this.playAudio("E")}>E<audio id="E" className="clip" src="audio/explosion.ogg"><source src="audio/explosion.mp3" /><source src="audio/explosion.ogg" /></audio></button>
-        <button id="btnHit" className="drum-pad" onClick={() => this.playAudio("A")}>A<audio id="A" className="clip" src="audio/hit.ogg"><source src="audio/hit.mp3" /><source src="audio/hit.ogg" /></audio></button>
-        <button id="btnJump" className="drum-pad" onClick={() => this.playAudio("S")}>S<audio id="S" className="clip" src="audio/jump.ogg"><source src="audio/jump.mp3" /><source src="audio/jump.ogg" /></audio></button>
-        <button id="btnLaser" className="drum-pad" onClick={() => this.playAudio("D")}>D<audio id="D" className="clip" src="audio/laser.ogg"><source src="audio/laser.mp3" /><source src="audio/laser.ogg" /></audio></button>
-        <button id="btnPowerup" className="drum-pad" onClick={() => this.playAudio("Z")}>Z<audio id="Z" className="clip" src="audio/powerup.ogg"><source src="audio/powerup.mp3" /><source src="audio/powerup.ogg" /></audio></button>
-        <button id="btnSelect" className="drum-pad" onClick={() => this.playAudio("X")}>X<audio id="X" className="clip" src="audio/select.ogg"><source src="audio/select.mp3" /><source src="audio/select.ogg" /></audio></button>
-        <button id="btnStep" className="drum-pad" onClick={() => this.playAudio("C")}>C<audio id="C" className="clip" src="audio/step.ogg"><source src="audio/step.mp3" /><source src="audio/step.ogg" /></audio></button>
-    */
     return (
       <div id="drum-machine" className="App">
-        <div id="display"></div>
+        <div id="display" className="jumbotron">- - -</div>
         {buttons}
+        <div id="fade_1" className="col-sm-3">&nbsp;</div>
+        <div id="title" className="well col-sm-6">8-BIT Sound Board</div>
+        <div id="fade_2" className="col-sm-3">&nbsp;</div>
       </div>
     );
   }
