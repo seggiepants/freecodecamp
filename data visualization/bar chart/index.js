b1cc432c22c4cb9b8573cb25250a360458955ff1
seggiepants <41271733+seggiepants@users.noreply.github.com>
2020-03-14 00:21:07 -0600

    Didn't get far on the bar chart, I think some of the tests are junk.

--- data visualization/bar chart/index.js
+++ data visualization/bar chart/index.js
@@ -4,8 +4,8 @@ const h = 600;
 const url = "data/GDP-data.json";
 
 function createBarChart(data) {
-    const minDate = d3.min(data.data, (d) => Date.parse(d[0]));
-    const maxDate = d3.max(data.data, (d) => Date.parse(d[0]));
+    const minDate = d3.min(data.data, (d) => d[0]);
+    const maxDate = d3.max(data.data, (d) => d[0]);
     
     let minValue = d3.min(data.data, (d) => d[1]);
     const maxValue = d3.max(data.data, (d) => d[1]);
@@ -21,8 +21,16 @@ function createBarChart(data) {
         .attr("height", h)
         ;
 
+    const tooltip = d3.select("body")
+        .append("div")
+        .attr("class", "tooltip")
+        .attr("id", "tooltip")
+        .text("Hello World")
+        ;
+
+
     const padding = 60;
-    const xScale = d3.scaleLinear()
+    const xScale = d3.scaleTime()
         .domain([new Date(minDate), new Date(maxDate)])
         .range([padding, w - padding])
         ;
@@ -37,7 +45,7 @@ function createBarChart(data) {
         ;
         
     const xAxis = d3.axisBottom(xScale);
-    xAxis.tickFormat(d3.timeFormat("%Y"));
+    xAxis.tickFormat(d3.timeFormat("%m-%d-%Y"));
     const yAxis = d3.axisLeft(yScale);
 
     svg.append("g")
@@ -63,8 +71,26 @@ function createBarChart(data) {
         .attr("width", barWidth)
         .attr("height", (d, i) => yScale(minValue) - yScale(d[1])) 
         .attr("fill", "navy")
-        .attr("data-date", (d, i) => new Date(d[0]))
-        .attr("data-gdp", (d, i) => d[1]);  
+        .attr("data-date", (d, i) => d[0])
+        .attr("data-gdp", (d, i) => d[1])
+        .append("title")
+        .text(d => `GDP: ${d[1]}
+Date: ${new Date(d[0]).toLocaleDateString()}`)
+        .attr("data-date", d => new Date(d[0]))
+        .on("mouseover", (d, i) => {
+            console.log(`mouseover: ${d}, ${i}`);
+/*            tooltip
+                .text(`GDP: ${d[1]}
+Date: ${new Date(d[0]).toLocaleDateString()}`)
+                .style("visible", "true")
+                .attr("data-date", d => new Date(d[0]))
+        */})
+        .on("mouseout", (d, i) => { 
+            //console.log(`mouseout: ${d}, ${i}`);
+            console.log("mouseout");
+            //tooltip.style("visible", "false")
+        })
+        ;
 }
 
 fetch(url)
