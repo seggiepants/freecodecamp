2084daed95bd1641223612204b993b15e7839b17
seggiepants <41271733+seggiepants@users.noreply.github.com>
2020-04-05 23:38:10 -0600

    The error was I didn't set the content type to application/json oy!

--- apis and microservices/request header parser/app/server.js
+++ apis and microservices/request header parser/app/server.js
@@ -5,23 +5,24 @@ var cors = require('cors');
 var app = express();
 app.use(bodyParser.urlencoded({extend: false}));
 app.use(cors());
-app.use("/", express.static(__dirname + "/public"));
 
-app.get("/api/whoami", (req, res) => {  
+app.get("/api/whoami", (req, res) => {    
   console.log(req.headers);
   var results = {};
 
   // Fill in the ordinary keys.
+  /*
   var keys = Object.keys(req.headers);
   for (var i = 0; i < keys.length; i++) {
       results[keys[i]] = req.headers[keys[i]];
   }
+  */
 
   // IP Address
   if (!Object.hasOwnProperty("ipaddress")) {
-    results["ipaddress"] = req.ip || req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    results["ipaddress"] = req.ip; //req.header('x-forwarded-for') || req.connection.remoteAddress;; // || req.headers["x-forwarded-for"] || req.connection.remoteAddress;
   }
-
+  
   // Software == user-agent?
   if (!Object.hasOwnProperty("software")) {
     results["software"] = req.headers["user-agent"] || "unknown";
@@ -31,10 +32,11 @@ app.get("/api/whoami", (req, res) => {
   if (!Object.hasOwnProperty("language")) {
     results["language"] = req.headers["accept-language"] || "unknown";
   }
-
+  res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(results));
 });
 
+app.use("/", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
 var listener = app.listen(process.env.PORT || 3000 , () => {
