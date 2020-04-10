ecd2619ea0f81cfc6b74fb8945e5fb57bed20883
seggiepants <41271733+seggiepants@users.noreply.github.com>
2020-04-10 00:48:15 -0600

    Finally got things working. Passed the tests too.

--- apis and microservices/url shortener/app/server.js
+++ apis and microservices/url shortener/app/server.js
@@ -9,15 +9,20 @@ var cors = require('cors');
 var app = express();
 app.use(bodyParser.urlencoded({extend: false}));
 app.use(cors());
-
-const db = mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 var shortenerSchema = new mongoose.Schema({
     _id: Number,
     url:  {type: String, required: true}, // String is shorthand for {type: String}
   });
 
+var counterSchema = new mongoose.Schema({
+  _id: String
+  , sequence_value: Number,
+})
+
 const Shortener = mongoose.model('Shortener', shortenerSchema);
+const Counter = mongoose.model('Counter', counterSchema);
 
 /*
 look at: https://jelly-iris-pea.glitch.me
@@ -41,51 +46,47 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/shorturl/:id?", (req, res, next) => {
+app.get("/api/shorturl/:id?", (req, res) => {
   // Check to see if the id exists, if so, return the associated url.  
   if (req.params.id === null) {    
     return done('did not receive a url to lookup.');
-  } else if (!IsNaN(req.params.id)) {
-    Shortener.findById(parseInt(req.params.id), (error, data, next) => {
-      if (error) return done(error);
-      if (data.hasOwnProperty('url')) {
-        res.redirect(data.url);
-      } else {
-        return done(`cannot find a shortened value for ${req.params.id}.`);
+  } else if (!isNaN(req.params.id)) {
+    Shortener.findById(parseInt(req.params.id), (error, data) => {
+      if (error) return Promise.reject(error);
+      res.status(301);
+      let url = data.url;
+      if (url.search('://') == -1) {
+        url = 'http://' + url;
       }
-      next();
+      res.redirect(url);
     });
   } else {
-    return done("Unable to parse.");
-    next();
+    return res.json({error: "Unable to parse."});
   }
-});
-
-app.post("/api/shorturl/:id?", (req, res, next) => {
+}).post("/api/shorturl/:id?", (req, res) => {
   if (req.params.id == 'new') {
-    let newURL = decodeURIComponent(req.body);
-    dns.lookup(newURL, { family: 0, all: false}, (err, address, family) => {
-      if (err !== undefined && err !== null) {
-      let newID = getNextSequenceValue("shortener");
-      
-      let shortURL = Shortener({
-        _id: newID
-        , url: newURL
-      });
-      shortURL.save((error, data) => {
-        if (error) return done(error);
-        done(null, data);
-      });
-    
-      res.send({
-        original_url: newURL
-        ,short_url: newID
-        });
-      } else {
-        res.send({error: err});
+    let newURL = decodeURIComponent(req.body.url);
+    let url = new URL(newURL);
+    dns.lookup(url.hostname, { family: 0, all: false}, (err, address, family) => {
+      if (err === undefined || err === null) {
+        GetNextSequenceValue("shortener").then(data => {
+          let newCounter = data.sequence_value;
+          let shortURL = Shortener({
+            _id: newCounter
+            , url: newURL
+          });
+          shortURL.save((error, data) => {
+            if (error) return res.json({error: error});
+            res.json({
+            original_url: newURL
+            ,short_url: root_url + 'api/shorturl/' + newCounter
+            });
+          });
+        }
+      )} else {
+        res.json({error: err});
       }
     });
-    next();
   }
 });
 
@@ -93,14 +94,13 @@ app.listen(port, () => {
   console.log(`Application listening on port ${port} ...`);
 });
 
-function getNextSequenceValue(sequenceName){
-  console.log('db:');
-  console.log(db);
-  var sequenceDocument = db.counters.findOneAndUpdate({
-    query:{_id: sequenceName },
-    update: {$inc:{sequence_value:1}},
-    new:true
-  });
-  return sequenceDocument.sequence_value;
+async function GetNextSequenceValue(sequenceName){
+  return Counter.findOneAndUpdate({_id: sequenceName}, {$inc:{sequence_value:1}}, {new: true, upsert: true, useFindAndModify: false}, (error, data) => {
+    if (error) 
+      return Promise.reject({error: error});
+    else
+      return Promise.resolve(data);
+  })
 }
+
 
